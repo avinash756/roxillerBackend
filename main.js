d21fc30fd605498eb54558b766c1371ba82f63da
Avinash <avinashgangarapu122@gmail.com>
2025-07-11 23:33:55 +0530

    port changes

--- main.js
+++ main.js
@@ -407,6 +407,6 @@ FROM users u;
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
