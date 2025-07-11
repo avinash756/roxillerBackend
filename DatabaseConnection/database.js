d1a004ae827be062d3f67c9344449002e7674536
Avinash <avinashgangarapu122@gmail.com>
2025-07-11 21:43:30 +0530

    onlineDatabaseConnected2

--- DatabaseConnection/database.js
+++ DatabaseConnection/database.js
@@ -4,13 +4,11 @@ require("dotenv").config({
 const mysql = require("mysql2/promise");
 
 const connection = mysql.createPool({
-   host: process.env.DATABASE_HOST,
+  host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  port: process.env.DATABASE_PORT, 
+  port: process.env.DATABASE_PORT,
 });
 
-
 module.exports = connection;
-
